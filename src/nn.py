7bbbf0eca1b493546ad2073313d6a6a468c8c1cd
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2021-03-03 04:57:30 +0000

    fix: nn iris classifier

--- src/nn.py
+++ src/nn.py
@@ -3,18 +3,19 @@ import random
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.python.keras.activations import sigmoid
 
 import dataset
+import prettytable
 
 print('TF version', tf.version.VERSION)
 
-target = 'setosa'
+target = 'virginica'
 
 # Seed
 seed = 5
 random.seed(seed)
 np.random.seed(seed)
+tf.random.set_seed(seed)
 
 # Prepare dataset
 head, x, labels = dataset.from_file('./data/iris.csv')
@@ -26,16 +27,23 @@ x_train, x_test = dataset.split(x, ratio=0.9)
 y_train, y_test = dataset.split(labels, ratio=0.9)
 
 model = keras.models.Sequential([
-    keras.layers.Dense(4, activation='sigmoid'),
-    keras.layers.Dense(4, activation='sigmoid'),
-    keras.layers.Dense(1, activation='softmax')
+    keras.layers.Flatten(input_shape=(4,)),
+    keras.layers.Dense(8, activation=keras.activations.relu),
+    keras.layers.Dense(1, activation=keras.activations.sigmoid)
 ])
 
 model.compile(optimizer='adam',
               loss=keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
 
-fit = model.fit(x_train, y_train, epochs=5)
-print('History (accuraty):', fit.history['accuracy'])
+model.fit(x_train, y_train, epochs=10, batch_size=1)
 
-eval = model.evaluate(x_test, y_test)
-print(f'Loss = {eval[0]}, accuracy = {eval[1]}')
+model.evaluate(x_test, y_test)
+
+table = prettytable.PrettyTable(
+    ['Actual', 'Predicted value', 'Predicted label'])
+
+predictions = model.predict(x_test)
+for actual, predicted in zip(y_test, predictions):
+    table.add_row([actual, int(predicted[0] > 0.5), predicted[0]])
+
+print(table)
