d3cd8fe1d3534ccddfc70df1c098d48877302171
Ivan <korotkov.ivan.s@gmail.com>
2021-03-28 17:23:12 +0500

    feat: build signature recursively

--- src/linguistic.py
+++ src/linguistic.py
@@ -1,27 +1,28 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Union
 import numpy as np
 
-class Group:
-    def __init__(self) -> None:
-        self._items = []
-
-    def add(self, code: int):
-        self._items.append(code)
-
-    def __str__(self) -> str:
-        sequence = ''.join(map(str, self._items))
-        return f'({sequence})'
+class Signature:
+    pass
 
 class Signature:
-    def __init__(self) -> None:
-        self._sequences = []
+    def __init__(self, code: int = None) -> None:
+        self._code: int = code
+        self._children: List[Signature] = []
 
-    def add(self, group: Group):
-        self._sequences.append(group)
+    def add(self, s: Union[Signature, int]):
+        self._children.append(s)
 
     def __str__(self) -> str:
-        return ''.join(map(str, self._sequences))
+        if len(self._children) > 1:
+            s = map(lambda x: f'({x})', self._children)
+            s = ''.join(map(str, s))
+        elif len(self._children) == 1:
+            child = self._children[0]
+            s = f'({child})' if not self._code else str(child) if child._code else ''
+        else:
+            s = str(self._code)
 
+        return f'{self._code}{s}' if self._code else f'{s}'
 
 class Linguistic:
     model: Dict[str, Signature]
@@ -52,40 +53,40 @@ class Linguistic:
         return (0, 0), False
 
 
-    def _directions(self, img: np.ndarray, row: int, col: int) -> Tuple[int, List[Tuple[int, int]], bool]:
+    def _directions(self, img: np.ndarray, row: int, col: int) -> Tuple[int, List[Tuple[int, int]]]:
         # 8 1 2
         # 7 . 3
         # 6 5 3
 
-        direction_code = 9
+        direction_code = 0
         directions = []
 
         if row > 0 and img[row-1, col] > 0:
             directions.append((row-1, col))
-            direction_code = min(direction_code, 1)
+            direction_code = 1 if direction_code == 0 else direction_code
         if row > 0 and col < img.shape[1] - 1 and img[row-1, col+1] > 0:
             directions.append((row-1, col+1))
-            direction_code = min(direction_code, 2)
+            direction_code = 2 if direction_code == 0 else direction_code
         if col < img.shape[1] - 1 and img[row, col+1] > 0:
             directions.append((row, col+1))
-            direction_code = min(direction_code, 3)
+            direction_code = 3 if direction_code == 0 else direction_code
         if row < img.shape[0] - 1 and col < img.shape[1] - 1 and img[row+1, col+1] > 0:
             directions.append((row+1, col+1))
-            direction_code = min(direction_code, 4)
+            direction_code = 4 if direction_code == 0 else direction_code
         if row < img.shape[0] - 1 and img[row + 1, col] > 0:
             directions.append((row+1, col))
-            direction_code = min(direction_code, 5)
+            direction_code = 5 if direction_code == 0 else direction_code
         if row < img.shape[0] - 1 and col > 0 and img[row + 1, col-1] > 0:
             directions.append((row+1, col-1))
-            direction_code = min(direction_code, 6)
+            direction_code = 6 if direction_code == 0 else direction_code
         if col > 0 and img[row, col-1] > 0:
             directions.append((row, col-1))
-            direction_code = min(direction_code, 7)
+            direction_code = 7 if direction_code == 0 else direction_code
         if row > 0 and col > 0 and img[row-1, col-1] > 0:
             directions.append((row-1, col-1))
-            direction_code = min(direction_code, 8)
+            direction_code = 8 if direction_code == 0 else direction_code
 
-        return direction_code, directions, direction_code != 9
+        return direction_code, directions
 
 
     def _signature(self, img: np.ndarray) -> Signature:
@@ -97,29 +98,27 @@ class Linguistic:
             if not ok:
                 break
 
-            paths = [start]
-            while len(paths) > 0:
-                row, col = paths.pop(0)
-                if img[row, col] == 0:
-                    continue
-
-                directions = [(row, col)]
-                group = Group()
-
-                while len(directions) > 0:
-                    row, col = directions.pop(0)
-                    img[row, col] = 0
-
-                    code, directions, ok = self._directions(img, row, col)
-                    if not ok:
-                        break
-
-                    paths.extend(directions[1:])
-                    group.add(code)
-
-                sign.add(group)
+            row, col = start
+            if img[row, col] > 0:
+                s = self._signature_recurse(img, start)
+                sign.add(s)
 
         return sign
+
+    def _signature_recurse(self, img: np.ndarray, start: Tuple[int, int]) -> Signature:
+        row, col = start
+        img[row, col] = 0
+
+        code, directions = self._directions(img, row, col)
+        sign = Signature(code)
+
+        for row, col in directions:
+            if img[row, col] > 0:
+                alt_sign = self._signature_recurse(img, (row, col))
+                sign.add(alt_sign)
+
+        return sign
+
 
     def _normalize(img: List[int]) -> List[int]:
         pass
