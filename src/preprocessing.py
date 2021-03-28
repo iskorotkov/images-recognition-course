6b5bcf4d07fa8a6b97dbde71680e1a41f749bf34
Ivan <korotkov.ivan.s@gmail.com>
2021-03-28 19:06:01 +0500

    feat: add support for reading processed images

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,6 @@
 from operator import contains
 from typing import List, Dict, Tuple
+from shutil import Error
 import cv2
 import os
 import shutil
@@ -19,15 +20,35 @@ def process(source: str, destination: str, dimensions: Tuple[int, int]) -> None:
     save_images(destination, images)
 
 
-def load_images(path: str) -> Dict[str, np.array]:
+def load_images(path: str, nesting=3) -> Dict[str, List[np.ndarray]]:
     images = {}
-    for guid in os.listdir(path):
-        guidPath = os.path.join(path, guid)
-        for className in os.listdir(os.path.join(guidPath)):
+
+    if nesting == 3:
+        for guid in os.listdir(path):
+            guidPath = os.path.join(path, guid)
+            for className in os.listdir(os.path.join(guidPath)):
+                if not contains(images, className):
+                    images[className] = []
+
+                classPath = os.path.join(guidPath, className)
+                for file in os.listdir(classPath):
+                    originalFile = os.path.join(classPath, file)
+                    tempFile = os.path.join(classPath, '.tmp')
+
+                    shutil.copy2(originalFile, tempFile)
+                    image = cv2.imread(tempFile, cv2.IMREAD_UNCHANGED)
+                    os.remove(tempFile)
+
+                    if image is None:
+                        raise Exception(f'Couldn\'t read image at {originalFile}')
+
+                    images[className].append(image)
+    elif nesting == 2:
+        for className in os.listdir(os.path.join(path)):
             if not contains(images, className):
                 images[className] = []
 
-            classPath = os.path.join(guidPath, className)
+            classPath = os.path.join(path, className)
             for file in os.listdir(classPath):
                 originalFile = os.path.join(classPath, file)
                 tempFile = os.path.join(classPath, '.tmp')
@@ -40,11 +61,13 @@ def load_images(path: str) -> Dict[str, np.array]:
                     raise Exception(f'Couldn\'t read image at {originalFile}')
 
                 images[className].append(image)
+    else:
+        raise Error('Unsupported nesting level; only 2 and 3 levels are supported')
 
     return images
 
 
-def save_images(path: str, images: Dict[str, np.array]) -> None:
+def save_images(path: str, images: Dict[str, List[np.ndarray]]) -> None:
     for className, imagesList in images.items():
         folder = os.path.join(path, className)
         os.makedirs(folder, exist_ok=True)
@@ -54,11 +77,11 @@ def save_images(path: str, images: Dict[str, np.array]) -> None:
             cv2.imwrite(file, image)
 
 
-def resize(image: np.array, dim: Tuple[int, int]) -> np.array:
+def resize(image: np.ndarray, dim: Tuple[int, int]) -> np.ndarray:
     return cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
 
-def to_bgr(image: np.array) -> np.array:
+def to_bgr(image: np.ndarray) -> np.array:
     image = np.copy(image)
     if image.shape[2] == 4 and np.max(image[:, :, 3]) - np.min(image[:, :, 3]) >= 128:
         alpha = image[:, :, 3]
@@ -69,7 +92,7 @@ def to_bgr(image: np.array) -> np.array:
     return image
 
 
-def to_monochrome(image: np.array) -> np.array:
+def to_monochrome(image: np.ndarray) -> np.ndarray:
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, image = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
 
@@ -85,7 +108,7 @@ def to_monochrome(image: np.array) -> np.array:
     return image
 
 
-def crop(image: np.array) -> np.array:
+def crop(image: np.ndarray) -> np.ndarray:
     ratio = image.shape[1] / image.shape[0]  # columns / rows
     monochrome = to_monochrome(image)
 
